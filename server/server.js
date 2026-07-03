2b12839ae4c865bacaa863948b8e4f9e944ba610
MAC <mac@MACs-MacBook-Pro.local>
2026-07-03 12:25:08 +0530

    completed 1

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
